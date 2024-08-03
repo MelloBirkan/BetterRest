f03198c5a43c5ca0992b56598d8f550ec1766ed3
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-08-03 17:08:05 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Marcello Gonzatto Birkan on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
